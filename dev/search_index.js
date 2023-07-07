ee6a4430b09b930765bcf6705909d737a548fe78
Documenter.jl <documenter@juliadocs.github.io>
2023-07-07 09:11:07 +0000

    build based on c45d1d8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlaygroundWithGit","category":"page"},{"location":"#PlaygroundWithGit","page":"Home","title":"PlaygroundWithGit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlaygroundWithGit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlaygroundWithGit]","category":"page"}]
+}
