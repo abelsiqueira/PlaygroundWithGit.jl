d7a77b8f2dccd33492729b3d501955f231d38b8e
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-07-07 23:20:28 +0200

    Allow parameters in the loan interest calculator

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using PlaygroundWithGit
 using Test
 
 @testset "PlaygroundWithGit.jl" begin
-  x, fx = loan_interest_calculator()
+  x, fx = loan_interest_calculator(500_000.0, 360, 2700.0)
   @test x ≈ 0.0042 atol = 1e-5
   @test fx ≈ 0 atol = 1e-5
 end
