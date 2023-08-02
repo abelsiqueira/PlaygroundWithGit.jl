a26fd46dacde3fd50ee98a76e0de29468879d347
Lauren Clisby <lauren.clisby@tno.nl>
2023-08-02 11:00:56 +0200

    Issue #5 and updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,14 @@
+"""
+Before running this, you might have to run in the Julia REPL:
+using Pkg
+Pkg.instantiate()
+"""
+
 using PlaygroundWithGit
 using Test
 
 @testset "PlaygroundWithGit.jl" begin
-  x, fx = loan_interest_calculator(; verbose = true, rtol = 1e-12, atol = 1e-12)
+  x, fx = loan_interest_calculator(500_000.0, 360, 2700.0; verbose = true, rtol = 1e-12, atol = 1e-12)
   @test x ≈ 0.0042 atol = 1e-5
   @test fx ≈ 0 atol = 1e-5
 end
