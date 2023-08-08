00cef252e5f617f4fd4d57a2f9f08c25d921cb93
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-08-08 14:05:30 +0200

    Creating conflicts

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using PlaygroundWithGit
 using Test
 
 @testset "PlaygroundWithGit.jl" begin
-  x, fx = loan_interest_calculator(; verbose = true)
+  x, fx = loan_interest_calculator(; verbose = true, rtol = 1e-12, atol = 1e-12)
   @test x ≈ 0.0042 atol = 1e-5
   @test fx ≈ 0 atol = 1e-5
 end
